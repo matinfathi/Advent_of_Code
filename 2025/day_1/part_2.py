dceecfc587b20927e3e3b1015bce135e31f8e239
Matin Fathi <matinfathi1998@gmail.com>
2025-12-06 18:56:49 +0330

    update

--- 2025/day_1/part_2.py
+++ 2025/day_1/part_2.py
@@ -1,4 +1,4 @@
-FILE_ADDRESS = "2025/day_1/test.txt"
+FILE_ADDRESS = "2025/day_1/input_day_1.txt"
 
 
 def open_file(file_address: str) -> list[str]:
@@ -22,29 +22,29 @@ def main():
     num_zeros = 0
 
     lines = open_file(file_address=FILE_ADDRESS)
-    for item in lines:
-        number = int(item[1:].strip())
 
-        old_total = total
+    for item in lines:
+        number = abs(int(item[1:].strip()))
+        to_99 = 100 - total
+
+        num_zeros += number // 100
+        extra = number % 100
 
         if item.startswith("R"):
-            total += number
+            if extra <= to_99:
+                total += extra
+            else:
+                num_zeros += 1
+                total = extra - to_99
+
         else:
-            total -= number
+            if extra < total:
+                total -= extra
+            else:
+                num_zeros += 1
+                total = 100 - (extra - total)
 
-        change_in_hundreds = abs(get_hundred_digit(total) - get_hundred_digit(old_total))
-
-        if total == 0:
-            # print(item)
-            num_zeros += 1
-
-        if change_in_hundreds:
-            num_zeros += change_in_hundreds
-        else:
-            if total * old_total < 0:
-                num_zeros += change_in_hundreds
-
-        print(total)
+            
 
 
     print(total)
